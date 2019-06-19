9e7a62258d58479abe2da7ec72460a5bae1b240b
ibmdev <ibmdev102017@gmail.com>
2019-06-19 18:47:18 +0200

    test

--- src/scripts/update.package.ts
+++ src/scripts/update.package.ts
@@ -1,4 +1,4 @@
-import { commit, discard, getBranch, push, stage, unstage } from '../manager/git.manager';
+import { discard, getBranch, push, stage, unstage } from '../manager/git.manager';
 import { updateVersionPatch } from '../manager/node.manager';
 
 async function main() {
@@ -7,7 +7,6 @@ async function main() {
   await updateVersionPatch();
   await unstage();
   await stage('package.json package-lock.json');
-  await commit('Release package.json', 'package.json package-lock.json');
   await push(branch);
 }
 main();
