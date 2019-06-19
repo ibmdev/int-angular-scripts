99809f4fb709075e76efb8d3680c7aaafd58bbaf
ibmdev <ibmdev102017@gmail.com>
2019-06-19 19:29:38 +0200

    Prise en compte de la fonctionnalité de tags automatique

--- lib/manager/node.manager.d.ts
+++ lib/manager/node.manager.d.ts
@@ -1,12 +1,12 @@
-export declare function updateVersionPatch(): Promise<{
+export declare function updateVersionPatch(tag?: boolean): Promise<{
     stdout: string;
     stderr: string;
 }>;
-export declare function updateVersionMinor(): Promise<{
+export declare function updateVersionMinor(tag?: boolean): Promise<{
     stdout: string;
     stderr: string;
 }>;
-export declare function updateVersionMajor(): Promise<{
+export declare function updateVersionMajor(tag?: boolean): Promise<{
     stdout: string;
     stderr: string;
 }>;
--- src/manager/node.manager.ts
+++ src/manager/node.manager.ts
@@ -2,12 +2,21 @@ import * as child_process from 'child_process';
 import { promisify } from 'util';
 const exec = promisify(child_process.exec);
 
-export function updateVersionPatch() {
+export function updateVersionPatch(tag?: boolean) {
+  if (tag) {
+    return exec('npm version patch --force');
+  }
   return exec('npm version patch --force --no-git-tag-version');
 }
-export function updateVersionMinor() {
+export function updateVersionMinor(tag?: boolean) {
+  if (tag) {
+    return exec('npm version minor --force');
+  }
   return exec('npm version minor --force --no-git-tag-version');
 }
-export function updateVersionMajor() {
+export function updateVersionMajor(tag?: boolean) {
+  if (tag) {
+    return exec('npm version major --force');
+  }
   return exec('npm version major --force --no-git-tag-version');
 }
