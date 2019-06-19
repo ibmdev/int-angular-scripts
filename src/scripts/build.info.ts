89a7c47df7f627b4fca063905c30315cac1b4b30
ibmdev <ibmdev102017@gmail.com>
2019-06-19 18:09:36 +0200

    Attente de 5 secondes avant de commiter

--- src/scripts/build.info.ts
+++ src/scripts/build.info.ts
@@ -1,5 +1,6 @@
 import { createBuildInfo } from '../manager/filesystem.manager';
 import { commit, getBranch, getRevision, push, stage, unstage } from '../manager/git.manager';
+import { attente } from '../manager/timer.manager';
 
 async function main() {
   const branch = (await getBranch()).stdout.toString().trim();
@@ -9,6 +10,7 @@ async function main() {
   createBuildInfo('src/environments/build_info.ts', info);
   await unstage();
   await stage('src/environments/build_info.ts');
+  await attente(5000);
   await commit('Release build-info', 'src/environments/build_info.ts');
   await push(branch);
 }
