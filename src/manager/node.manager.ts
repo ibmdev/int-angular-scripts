91d028d6f123af956bdff0add6e8e887aad7803b
ibmdev <ibmdev102017@gmail.com>
2019-06-19 19:06:06 +0200

    Disable automatic git tag

--- src/manager/node.manager.ts
+++ src/manager/node.manager.ts
@@ -3,11 +3,11 @@ import { promisify } from 'util';
 const exec = promisify(child_process.exec);
 
 export function updateVersionPatch() {
-  return exec('npm version patch --force');
+  return exec('npm version patch --force --no-git-tag-version');
 }
 export function updateVersionMinor() {
-  return exec('npm version minor --force');
+  return exec('npm version minor --force --no-git-tag-version');
 }
 export function updateVersionMajor() {
-  return exec('npm version major --force');
+  return exec('npm version major --force --no-git-tag-version');
 }
